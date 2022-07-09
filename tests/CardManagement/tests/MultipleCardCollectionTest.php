b700dcd1345369bdc9977a5beb8f089689cbb8aa
Phillip Rearick <psrearick@gmail.com>
2022-07-09 19:41:04 -0400

    wip

--- tests/CardManagement/tests/MultipleCardCollectionTest.php
+++ tests/CardManagement/tests/MultipleCardCollectionTest.php
@@ -20,25 +20,34 @@ test('a collection can retrieve its cards', function () {
     $this->assertEquals(3, $cards[0]['parameters']['quantity']);
 });
 
-//test('a collection can have multiple cards',
-//    /**
-//     * @throws JsonException
-//     */
-//    function () {
-//        $card       = Card::factory()->create();
-//        $collection = Collection::factory()->create();
-//        $parameters = new ContainerItemParameters();
-//
-//        $parameters->set('quantity', 3);
-//
-//        app(Containers::class)->addItemToContainer($card, $collection, $parameters);
-//
-//        $parameters->set('quantity', 2);
-//
-//        app(Containers::class)->addItemToContainer($card, $collection, $parameters);
-//
-//        $setParameters = app(Containers::class)->getParameters($card, $collection);
-//
-//        $this->assertEquals(3, $setParameters->get('quantity'));
-//    }
-//);
+test('a collection can have multiple cards', function () {
+    $card       = Card::factory()->create();
+    $collection = Collection::factory()->create();
+    $parameters = new ContainerItemParameters();
+
+    $parameters->set('quantity', 3);
+
+    app(Containers::class)->addItemToContainer($card, $collection, $parameters);
+
+    $card2 = Card::factory()->create();
+    $parameters->clear();
+    $parameters->set('quantity', 2);
+
+    app(Containers::class)->addItemToContainer($card2, $collection, $parameters);
+
+    $cards = app(Containers::class)->getContainerItems($collection, Card::class);
+
+    $this->assertCount(2, $cards);
+    $this->assertEquals(3, $cards[0]['parameters']['quantity']);
+    $this->assertEquals($card->price, $cards[0]['parameters']['price']);
+    $this->assertEquals(2, $cards[1]['parameters']['quantity']);
+    $this->assertEquals($card2->price, $cards[1]['parameters']['price']);
+    $this->assertEquals(($card->price * 3) + ($card2->price * 2), $collection->value);
+
+    $parameters->clear();
+    $parameters->set('quantity', -1);
+
+    app(Containers::class)->removeItemFromContainer($card, $collection, $parameters);
+
+    $this->assertEquals(($card->price * 2) + ($card2->price * 2), $collection->value);
+});
