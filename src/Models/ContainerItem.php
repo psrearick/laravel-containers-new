68f5a8da760f413b98526a9831becd800eca236c
Phillip Rearick <psrearick@gmail.com>
2022-07-09 18:48:13 -0400

    wip

--- src/Models/ContainerItem.php
+++ src/Models/ContainerItem.php
@@ -4,6 +4,14 @@ namespace Psrearick\Containers\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property int $container_id
+ * @property string $container_type
+ * @property int $item_id
+ * @property string $item_type
+ * @property string $parameters
+ */
 class ContainerItem extends Model
 {
     protected $guarded = [];
